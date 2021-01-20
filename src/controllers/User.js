628227d09968ba5a2128f749a49febb33e568b10
Carlos Henrique <carllosshennriique@gmail.com>
2021-01-20 16:06:08 -0300

    Buscar, atualizar e deletar usuario implementados

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -27,7 +27,52 @@ module.exports = {
             return res.send(users);
         } catch (erro) {
             return res.status(400).send({
-                message: "Não foi possivel lisar usuários",
+                message: "Não foi possivel listar usuários",
+                erro: erro,
+            });
+        }
+    },
+
+    getUser: async (req, res) => {
+        try {
+            const id = req.params.id;
+
+            const user = await User.findOne({ _id: id });
+
+            return res.send(user);
+        } catch (erro) {
+            return res.status(400).send({
+                message: "Não foi possivel listar usuário",
+                erro: erro,
+            });
+        }
+    },
+
+    update: async (req, res) => {
+        try {
+            const id = req.params.id;
+
+            await User.updateOne({ _id: id }, req.body);
+
+            return res.send(req.body);
+        } catch (erro) {
+            return res.status(400).send({
+                message: "Não foi possivel atualizar usuário",
+                erro: erro,
+            });
+        }
+    },
+
+    delete: async (req, res) => {
+        try {
+            const id = req.params.id;
+
+            await User.deleteOne({ _id: id });
+
+            res.send({ message: `Usuário com id ${id} excluído com sucesso` });
+        } catch (erro) {
+            return res.status(400).send({
+                message: "Não foi possivel deletar usuário",
                 erro: erro,
             });
         }
--- src/routers/User.js
+++ src/routers/User.js
@@ -4,4 +4,10 @@ module.exports = (app) => {
     app.post("/userRegister", userControllers.register);
 
     app.get("/getUsers", userControllers.getUsers);
+
+    app.get("/getUser/:id", userControllers.getUser);
+
+    app.put("/updateUser/:id", userControllers.update);
+
+    app.delete("/deleteUser/:id", userControllers.delete);
 };

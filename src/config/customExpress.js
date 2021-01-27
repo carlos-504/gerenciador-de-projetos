cc1ca097c5a4cfba2b61422feeaa752be0d001f9
Carlos Henrique <carllosshennriique@gmail.com>
2021-01-27 17:54:06 -0300

    Cors habilitado para requisiçoes

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const bodyPaser = require("body-parser");
+const app = express();
 
 app.use(bodyPaser.json());
 app.use(bodyPaser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    app.use(cors());
+
+    next();
+});
 
 const routers = require("../routers");
 routers(app);

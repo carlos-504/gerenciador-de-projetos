7f136e298291ca51ae2be473f39204c4597fefca
Carlos Henrique <carllosshennriique@gmail.com>
2021-01-27 19:59:32 -0300

    Corrigindo erro de configuraçao do cors

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -6,14 +6,13 @@ const app = express();
 app.use(bodyPaser.json());
 app.use(bodyPaser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    app.use(cors());
-
-    next();
-});
-
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+        exposedHeaders: ["set-cookie"],
+    })
+);
 const routers = require("../routers");
 routers(app);
 
